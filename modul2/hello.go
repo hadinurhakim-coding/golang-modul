500d069379b24d70aaa7f509d9f50c6d009bfa96
hadiK1m <haadinurhakim@gmail.com>
2026-03-12 11:03:36 +0700

    Update hello.go to use fmt package for output and add string manipulation examples. Enhance comments for clarity on functionality.

--- modul2/hello.go
+++ modul2/hello.go
@@ -1,6 +1,15 @@
 package main
 
+// Import package fmt untuk menampilkan pesan ke console
+import "fmt"
+
 func main() {
 	// Menampilkan pesan "Hello, World!"
-	println("Hello, World!")
+	fmt.Println("Hello, World!")
+
+	// Menampilkan karakter pertama dari string "Halo, Nama saya Hadi Nurhakim"
+	fmt.Println("Halo, Nama saya Hadi Nurhakim"[0])
+
+	// Menampilkan panjang dari string "Halo, Nama saya Hadi Nurhakim"
+	fmt.Println(len("Halo, Nama saya Hadi Nurhakim"))
 }
